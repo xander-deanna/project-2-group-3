361c8be3e7413d0e941fb57bed7542aabc0d2aa4
Walter M <waltermccone88@gmail.com>
2021-04-08 09:16:43 -0500

    added some comments to the index and the User interest models

--- models/index.js
+++ models/index.js
@@ -2,13 +2,13 @@ const Users = require('./Users');
 const Interests = require('./Interests');
 const UserInterests = require('./UserInterests')
 
-// Interests belongsTo Users
+// Interests belongsTo many users, uses the User Interests table as the through point
 Interests.belongsToMany(Users, {
     through: UserInterests,
     foreignKey: 'user_id',
   });
 
-// Users have many Interests
+// Users should belong to many interests, uses the User Interests table as the through point
 Users.belongsToMany(Interests, {
     through: UserInterests,
     foreignKey: 'interest_id'
