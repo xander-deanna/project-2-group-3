841b922891fc69ad1d5da9083b58ef1f4c68bf7a
Deanna <xander.deanna@gmail.com>
2021-04-16 14:20:16 -0500

    added interest routes and front end logic to display interests on profile page and allow users to add interests.

--- controllers/api/interestRoutes.js
+++ controllers/api/interestRoutes.js
@@ -1,13 +1,29 @@
 const router = require('express').Router();
 const { Users, Interests, UserInterests } = require('../../models');
 
-router.get('/', (req,res) => {
-    Interests.findAll()
-    .then((interest) => res.render("profile",{interest: interest}))
-.catch((err) =>{
-    console.log(err);
-})
-})
+// router.get('/', (req,res) => {
+//     Interests.findAll()
+//     .then((interest) => res.render("profile",{interest: interest}))
+// .catch((err) =>{
+//     console.log(err);
+// })
+// })
+
+router.post('/addInterest', async (req,res) => {
+  try {
+    const newInterest = await UserInterests.create({
+      user_id: req.body.userIdBtn,
+      interest_id: req.body.interestIdBtn,
+    });
+    if (!newInterest) {
+      res.status(404).json({ message: 'no information was sent, please try again.' });
+      return;
+    }
+    res.status(200).json(newInterest);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
 
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,25 +1,46 @@
-// Get the modal
-var modal = document.getElementById("#interest-popup");
+// // Get the modal
+// var modal = document.getElementById("#interest-popup");
 
-// Get the button that opens the modal
-var btn = document.getElementById("#add-interest");
+// // Get the button that opens the modal
+// var btn = document.getElementById("#add-interest");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("#close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("#close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+// query selectors to help add interests when user clicks add interest buttons
+const addInterests = async (event) => {
+  console.log("hellooooooo");
+  event.preventDefault();
+  const userIdBtn = document.querySelector('#modal-btn').getAttribute('value');
+  const interestIdBtn = event.target.getAttribute('data-id');
+
+  const response = await fetch('/api/interests/addInterest', {
+    method: 'POST',
+    body: JSON.stringify({userIdBtn, interestIdBtn}),
+    headers: {'Content-Type': 'application/json'},
+  });
+  if (response.ok) {
+    document.location.reload();
   }
-}
+};
+document
+  .querySelectorAll('.interest-btn').forEach(button => {
+    button.addEventListener('click', addInterests);
+  });
